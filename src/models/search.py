6ed96991724d8cd56b8b8b07bd7c3d688a01eace
GFlyan <guilhermeflyan.22@gmail.com>
2025-05-01 19:25:30 -0300

    update search.themes()
    Correções na funcionalidade de pesquisa por filtragem de temas.

--- src/models/search.py
+++ src/models/search.py
@@ -1,8 +1,9 @@
 import ipeadatapy as ipea
 import pandas as pd
+   
 def theme(phrase: str):
     """
-    Retorna um dataframe contendo as series do IPEA de acordo com a string parametrizada referente ao tema procurado.
+    Retorna um dataframe contendo as series com dados financeiros do IPEA de acordo com a string parametrizada referente ao tema procurado.
 
     Caso a busca não seja bem sucedida sera retornado uma string "Não Encotrado".
     """
@@ -13,10 +14,11 @@ def theme(phrase: str):
         found = pd.DataFrame()
         for id in getThemeID['ID']:
             find = ipea.metadata(theme_id=id)
+            find = find[find['MEASURE'].str.contains("\\$")]
             found = pd.concat([found, find], ignore_index=True)
         found = found.sort_values(by='CODE')
     except:
-        return str("Não Encontrado")
+        return "Não Encontrado"
     else:
-        return found
+        return "Não Encontrado" if found.empty else found
     
